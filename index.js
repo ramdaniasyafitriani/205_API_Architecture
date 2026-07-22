f02d6f0b6763638516ee934b4c36f02e7bc41865
ramdaniasyafitriani <ramdaniania04@gmail.com>
2026-07-22 15:50:08 +0700

    feat: set up middleware for JSON and URL-encoded data handling

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const connectDatabase = require('./config/db');
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', require('./routes/api'));
