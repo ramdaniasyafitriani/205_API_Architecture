12ee0614cf9d1359a820b1e20aca3418fa6c3a6b
ramdaniasyafitriani <ramdaniania04@gmail.com>
2026-07-22 16:09:19 +0700

    feat: add Sequelize model initialization and update controller import path

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const komikController = require('../controllers/komikController');
+const komikController = require('../controller/komikController');
 
 router.get('/komik', komikController.getAllKomik);
 router.get('/komik/:id', komikController.getKomikById);
